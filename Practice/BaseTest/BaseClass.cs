9a63f881b8fa41844720d58c6683f9c9e91471ff
lesperon <luis.esperon@4thsource.com>
2019-11-09 20:51:31 -0600

    Add project files.

---
+++ Practice/BaseTest/BaseClass.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.BaseTest
+{
+    class BaseClass
+    {
+        public static IWebDriver driver;
+
+        private static string strUrlName = ConfigurationManager.AppSettings.Get("webaddress");
+
+        public static void Setup()
+        {
+
+            driver = new ChromeDriver();
+
+            driver.Url = strUrlName;
+            
+ 
+        }
+
+        public static void SendCredentials(By by,String pstrText)
+        {
+            driver.FindElement(by).Clear();
+            driver.FindElement(by).SendKeys(pstrText);
+            
+
+
+        }
+
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program : BaseTest.BaseClass
+    {
+        static void Main(string[] args)
+        {
+            Setup();
+
+            SendCredentials(By.Name("userName"), ConfigurationManager.AppSettings.Get("username"));
+            SendCredentials(By.Name("password"), ConfigurationManager.AppSettings.Get("password"));
+        }
+    }
+}
